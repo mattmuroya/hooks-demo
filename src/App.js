0a046069413a7a2ed8f9c52e9171da6813ff49c0
mattmuroya <matt.muroya@gmail.com>
2022-03-07 19:36:39 -1000

    hooks demo code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('black');
+
+  useEffect(() => {
+
+    const changeColorOnClick = () => { // helper function
+      if (color === 'black') {
+        setColor('red');
+      } else {
+        setColor('black');
+      }
+    };
+
+    document.getElementById('myDiv').addEventListener('click', changeColorOnClick); // runs on mount, and update (depending on dependency array)
+    
+    return () => { // component will unmount
+      document.getElementById('myDiv').removeEventListener('click', changeColorOnClick);
+    };
+  }, [color]); // if empty, works like component will mount. If filled, component will update (when dependency changes). If null, runs after initial render and on update.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div
+        id="myDiv"
+        style={{
+          color: 'white',
+          width: '100px',
+          height: '100px',
+          backgroundColor: color,
+        }}
+      >
+        This div can change color. Click on me!
+      </div>
   );
 }
 
